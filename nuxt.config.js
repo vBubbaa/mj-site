83913089df3fadd6612fc5bcf7330b30b9b21b22
vBubbaa <itylerjames@gmail.com>
2020-10-19 17:23:12 -0700

    overwrite generate to map blog files

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,5 +39,20 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-  }
+  },
+
+  target: 'static',
+
+  generate: {
+    routes: function() {
+      const fs = require('fs');
+      const path = require('path');
+      return fs.readdirSync('./content/blog').map(file => {
+        return {
+          route: `/blog/${path.parse(file).name}`, // Return the slug
+          payload: require(`./content/blog/${file}`),
+        };
+      });
+    },
+  },
 }
